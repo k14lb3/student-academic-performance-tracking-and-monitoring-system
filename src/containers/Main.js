a8399edc67bafbbabef2ee760724a621529bf775
Kialbe <k14lb3@gmail.com>
2021-04-14 17:51:41 +0800

    Add Register component to Route

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from 'hoc/PrivateRoute';
 import { useAuth } from 'contexts/AuthContext';
 import SignIn from './SignIn';
+import Register from './Register';
 import Home from './Home/Home';
 import Subjects from './Subjects/Subjects';
 import Settings from './Settings/Settings';
@@ -17,6 +18,7 @@ const Main = () => {
           <PrivateRoute path="/home" component={Home} />
           <PrivateRoute path="/subjects" component={Subjects} />
           <PrivateRoute path="/settings" component={Settings} />
+          <Route path="/register" component={Register} />
           <Route path="/sign-in" component={SignIn} />
           <Redirect to="/sign-in" />
         </Switch>
