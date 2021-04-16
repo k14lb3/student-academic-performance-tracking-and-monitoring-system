85a8a7715a1a3aca309885fb30e0c8d4ec120b0f
Kialbe <k14lb3@gmail.com>
2021-04-16 19:56:18 +0800

    Add content

--- src/containers/Settings/DeleteAccount.js
+++ src/containers/Settings/DeleteAccount.js
@@ -1,11 +1,23 @@
 import './DeleteAccount.scss';
 
 const DeleteAccount = () => {
-    return (
-        <div className="deleteAccount">
+  return (
+    <div className="deleteAccount">
+      <div>
+        <h3>This will delete your account</h3>
+        <p>You’re about to start the process of deleting your account.</p>
+        <h3>What else you should know</h3>
+        <ul>
+          <li>You cannot restore your account.</li>
+          <li>
+            Your information, e.g., name, grades, exams, etc, will not be
+            deleted from the other user's archived subjects.
+          </li>
+        </ul>
+      </div>
+      <button className="button">Delete</button>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default DeleteAccount
+export default DeleteAccount;
