520d8b4fbfc9bd47b2b84bbb6e62d9e508138032
Kialbe <k14lb3@gmail.com>
2021-04-20 23:15:11 +0800

    Make a variable for different variations to add readability to the code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,16 @@
+const VARIATIONS = {
+  DEFAULT:
+    'bg-yellow-500 text-gray-500 hover:bg-yellow-400 hover:border-yellow-400',
+  OUTLINED:
+    'bg-transparent text-orange-500 hover:bg-yellow-500 hover:bg-opacity-5',
+};
+
 const Button = ({ disabled, outlined, onClick, children }) => {
   return (
     <button
       disabled={disabled}
-      className={`disabled:opacity-80 py-2 px-6 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200${
-        outlined
-          ? ' bg-transparent text-orange-500 hover:bg-yellow-500 hover:bg-opacity-5'
-          : ' bg-yellow-500 text-gray-500 hover:bg-yellow-400 hover:border-yellow-400'
+      className={`disabled:opacity-80 py-2 px-6 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200 ${
+        outlined ? VARIATIONS.OUTLINED : VARIATIONS.DEFAULT
       }`}
       onClick={onClick}
     >
