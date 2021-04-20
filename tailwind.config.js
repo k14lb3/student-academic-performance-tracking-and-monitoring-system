fb3bbfa060d9a4bc5cd4aa5b3167a6b9f6f7a1d6
Kialbe <k14lb3@gmail.com>
2021-04-20 21:46:59 +0800

    Add media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       openSans: ['Open Sans', 'sans-serif'],
       roboto: ['Roboto', 'serif'],
     },
+    screens: {
+      xxx: { max: '336px' },
+      xs: { max: '480px' },
+      xm: { max: '624px' },
+      sm: { max: '768px' },
+      md: { max: '1024px' },
+      'r-md': { max: '1025px' },
+      'ol-sm': { min: '481px', max: '768px' },
+      'ol-md': { min: '769px', max: '1024px' },
+    },
     extend: {},
   },
   variants: {
