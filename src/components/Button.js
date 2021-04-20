529f27d40a1806379436c7b07388a46da1d33d8c
Kialbe <k14lb3@gmail.com>
2021-04-20 23:05:18 +0800

    Change padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ const Button = ({ disabled, outlined, onClick, children }) => {
   return (
     <button
       disabled={disabled}
-      className={`disabled:opacity-80 py-2 px-4 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200${
+      className={`disabled:opacity-80 py-2 px-6 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200${
         outlined
           ? ' bg-transparent text-orange-500 hover:bg-yellow-500 hover:bg-opacity-5'
           : ' bg-yellow-500 text-gray-500 hover:bg-yellow-400 hover:border-yellow-400'
