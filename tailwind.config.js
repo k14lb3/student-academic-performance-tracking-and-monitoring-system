7ac8fef26e4d649222b7934e961b3452398e0ef6
Kialbe <k14lb3@gmail.com>
2021-04-20 17:04:06 +0800

    Add font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      openSans: ['Open Sans', 'sans-serif'],
+      roboto: ['Roboto', 'serif'],
+    },
     extend: {},
   },
   variants: {
