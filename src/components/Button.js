c53f32d168a5ab779068c5c9638cbf340334c265
Kialbe <k14lb3@gmail.com>
2021-04-20 23:25:02 +0800

    Add props for full width and full height

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,11 +5,13 @@ const VARIATIONS = {
     'bg-transparent text-orange-500 hover:bg-yellow-500 hover:bg-opacity-5',
 };
 
-const Button = ({ disabled, outlined, onClick, children }) => {
+const Button = ({ disabled, wFull, hFull, outlined, onClick, children }) => {
   return (
     <button
       disabled={disabled}
-      className={`disabled:opacity-80 py-2 px-6 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200 ${
+      className={`disabled:opacity-80 ${wFull ? 'w-full' : ''} ${
+        hFull ? 'h-full' : ''
+      } py-2 px-6 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200 ${
         outlined ? VARIATIONS.OUTLINED : VARIATIONS.DEFAULT
       }`}
       onClick={onClick}
