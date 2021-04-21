e1d55ffe77935a3c11394da0c6c76a7c3c908fc0
Kialbe <k14lb3@gmail.com>
2021-04-21 23:27:11 +0800

    Delete scss styles and migrate to tailwindcss

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,4 +1,3 @@
-import './Home.scss';
 import { useEffect, useState } from 'react';
 import { useUser } from 'contexts/UserContext';
 import Loader from 'components/Loader';
@@ -14,20 +13,20 @@ const Home = () => {
   }, [userInfo]);
 
   return (
-    <div className="home">
-      <div className="home__header">
-        <h1 className="home__title">Home</h1>
+    <div className="w-full">
+      <div className="flex items-center py-5 xs:pt-0 xs:pb-3 border-solid border-b border-orange-500">
+        <h1 className="text-5xl xs:text-3xl">Home</h1>
       </div>
       {loading ? (
         <Loader />
       ) : (
-        <div className="home__body">
-          <h2 className="home__accountName">
+        <div className="py-5 xs:py-3">
+          <h2 className="text-3xl xs:text-lg">
             {`${userInfo.lastName}, ${userInfo.firstName} ${
               userInfo.middleName && `${userInfo.middleName[0]}.`
             }`}
           </h2>
-          <span className="home__accountType">{userInfo.type}</span>
+          <span className="text-2xl xs:text-base">{userInfo.type}</span>
         </div>
       )}
     </div>
