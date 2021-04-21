d4f2a70bb85fe11cf4c1fdf054fc42ae15d96e8e
Kialbe <k14lb3@gmail.com>
2021-04-21 13:09:13 +0800

    Change border radius

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,3 +1,5 @@
+import Loader from 'components/Loader';
+
 const VARIATIONS = {
   DEFAULT:
     'bg-yellow-500 text-gray-500 hover:bg-yellow-400 hover:border-yellow-400',
@@ -5,18 +7,31 @@ const VARIATIONS = {
     'bg-transparent text-orange-500 hover:bg-yellow-500 hover:bg-opacity-5',
 };
 
-const Button = ({ disabled, wFull, hFull, outlined, onClick, children }) => {
+const Button = ({
+  disabled,
+  hasLoader,
+  wFull,
+  hFull,
+  outlined,
+  onClick,
+  children,
+}) => {
   return (
     <button
       disabled={disabled}
-      className={`disabled:opacity-80 ${wFull ? 'w-full' : ''} ${
-        hFull ? 'h-full' : ''
-      } py-2 px-6 border-solid border border-orange-500 rounded-sm font-roboto font-semibold duration-200 ${
+      className={`disabled:opacity-80 ${
+        hasLoader ? 'relative flex justify-center items-center ' : ''
+      }${wFull ? 'w-full ' : ''}${
+        hFull ? 'h-full ' : ''
+      }py-2 px-6 border-solid border border-orange-500 rounded-md font-roboto font-semibold duration-200 ${
         outlined ? VARIATIONS.OUTLINED : VARIATIONS.DEFAULT
       }`}
       onClick={onClick}
     >
       {children}
+      {hasLoader.loading && (
+        <Loader inButton={{ outlined: outlined, width: hasLoader.width }} />
+      )}
     </button>
   );
 };
