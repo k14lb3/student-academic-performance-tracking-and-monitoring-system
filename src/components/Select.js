f394d769cb1ac0dc20722d2c000c3c8ae0b2be15
Kialbe <k14lb3@gmail.com>
2021-04-23 15:22:47 +0800

    Add Select component

---
+++ src/components/Select.js
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+
+const Select = ({ className, defaultValue, onChange, children }, ref) => {
+  return (
+    <select
+      ref={ref}
+      className={`bg-gray-50 px-3 py-2 text-black xs:text-xs border border-black rounded${
+        ` ${className}` || ''
+      }`}
+      defaultValue={defaultValue}
+      onChange={onChange}
+    >
+      {children}
+    </select>
+  );
+};
+
+export default forwardRef(Select);
