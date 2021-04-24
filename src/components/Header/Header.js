f36d5f953c3f62848077a3c068115e9de41c4811
Kialbe <k14lb3@gmail.com>
2021-04-24 14:35:02 +0800

    Add bottom margin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
           <header className="py-5 bg-gray xs:fixed xs:bottom-0 xs:w-full xs:py-0 xs:border-t xs:border-orange-500">
             <div className="h-full px-5 border-r xs:border-r-0 border-orange">
               {XS && (
-                <Link className="flex justify-start pt-5 pl-3" to="/home">
+                <Link className="flex justify-start pt-5 mb-5 pl-3" to="/home">
                   <img className="transform h-12 duration-200 hover:scale-110" src={logo} alt="" />
                 </Link>
               )}
