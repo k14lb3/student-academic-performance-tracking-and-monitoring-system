f9863ee31ce52e9ca981ab50339c177786bd649b
Kialbe <k14lb3@gmail.com>
2021-04-25 21:24:41 +0800

    Delete scss styles and migrate to tailwindcss

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,8 +1,10 @@
-import './Register.scss';
 import { useState, useRef } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { useAuth } from 'contexts/AuthContext';
-import Loader from 'components/Loader';
+import Button from 'components/Button/Button';
+import Input from 'components/Input';
+import Select from 'components/Select';
+import Error from 'components/Error';
 
 const SignIn = () => {
   const { user, register } = useAuth();
@@ -53,69 +55,100 @@ const SignIn = () => {
       {user ? (
         <Redirect to="/home" />
       ) : (
-        <div className="register">
-          <div className="register__header">
-            <h2>Create an account</h2>
+        <div className="p-10 xs:p-6 bg-gray shadow-xl xs:shadow-none">
+          <div className="pb-5 xs:pb-3 mb-5 xs:mb-3 border-b border-yellow-500">
+            <h2 className="text-3xl">Create an account</h2>
           </div>
           <div className="register__body">
             <form onSubmit={handleSubmit}>
-              <div className="input">
-                <label>First Name</label>
-                <input ref={firstNameRef} type="text" />
-              </div>
-              <div className="input">
-                <label>Last Name</label>
-                <input ref={lastNameRef} type="text" />
-              </div>
-              <div className="input">
-                <label>Middle Name</label>
-                <input ref={middleNameRef} type="text" />
-              </div>
-              <div className="input">
-                <label>Email</label>
-                <input ref={emailRef} type="text" />
-              </div>
-              <div className="register__password">
-                <div className="input">
-                  <label>Password</label>
-                  <input
-                    autoComplete="new-password"
+              <label
+                tw-content-after=" *"
+                className="content-after after:text-orange"
+              >
+                First Name
+              </label>
+              <Input ref={firstNameRef} className="w-full mb-3" />
+              <label
+                tw-content-after=" *"
+                className="content-after after:text-orange"
+              >
+                Last Name
+              </label>
+              <Input ref={lastNameRef} className="w-full mb-3" />
+              <label>Middle Name</label>
+              <Input ref={middleNameRef} className="w-full mb-3" />
+              <label
+                tw-content-after=" *"
+                className="content-after after:text-orange"
+              >
+                Email
+              </label>
+              <Input ref={emailRef} className="w-full mb-3" />
+              <div className="grid grid-cols-2 gap-5 mb-3">
+                <div>
+                  <label
+                    tw-content-after=" *"
+                    className="content-after after:text-orange"
+                  >
+                    Password
+                  </label>
+                  <Input
                     ref={passwordRef}
+                    className="w-full"
+                    type="password"
+                    autoComplete="new-password"
+                  />
+                </div>
+                <div>
+                  <label
+                    tw-content-after=" *"
+                    className="content-after after:text-orange"
+                  >
+                    Confirm Password
+                  </label>
+                  <Input
+                    ref={confirmPasswordRef}
+                    className="w-full"
                     type="password"
                   />
                 </div>
-                <div className="input">
-                  <label>Confirm password</label>
-                  <input ref={confirmPasswordRef} type="password" />
-                </div>
               </div>
-              <div>
-                <div className="input">
-                  <label>Account type</label>
-                  <select ref={typeRef}>
+              <div className="grid grid-cols-2 gap-5">
+                <div>
+                  <label
+                    tw-content-after=" *"
+                    className="content-after after:text-orange"
+                  >
+                    Account type
+                  </label>
+                  <Select ref={typeRef} className="w-full">
                     <option value="Instructor">Instructor</option>
                     <option value="Student">Student</option>
-                  </select>
+                  </Select>
                 </div>
-                <div className="input">
-                  <label>Gender</label>
-                  <select ref={genderRef}>
+                <div>
+                  <label
+                    tw-content-after=" *"
+                    className="content-after after:text-orange"
+                  >
+                    Gender
+                  </label>
+                  <Select ref={genderRef} className="w-full">
                     <option value="Male">Male</option>
                     <option value="Female">Female</option>
                     <option value="N/A">N/A</option>
-                  </select>
+                  </Select>
                 </div>
               </div>
-              {error && <span className="error">{error}</span>}
-              <button
-                disabled={loading}
-                className="signIn__signInButton button"
+              {error && <Error error={error} />}
+              <Button
+                className="w-full mb-1 mt-3"
+                hasLoader={{ loading: loading }}
               >
-                <span className={loading ? 'hidden' : ''}>Register</span>
-                {loading && <Loader />}
-              </button>
+                <span className={loading ? 'invisible' : ''}>Register</span>
+              </Button>
             </form>
-            <Link to="/sign-in" className="register__signInLink">
+            <Link to="/sign-in" className="text-orange hover:underline">
               Already have an account?
             </Link>
           </div>
