4509a70f1fa46863fc468539b613b249cf209c7f
Kialbe <k14lb3@gmail.com>
2021-04-25 09:26:07 +0800

    Wrap NavigationButton components with ul tag

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -5,9 +5,21 @@ import NavigationButton from 'components/Button/NavigationButton';
 const Navigation = () => {
   return (
     <nav className="flex flex-col xs:flex-row xs:justify-between xs:w-full py-5 xs:py-0 xs:px-5">
-      <NavigationButton to="/home" icon={faHome} title="Home" />
-      <NavigationButton to="/subjects" icon={faStickyNote} title="Subjects" />
-      <NavigationButton to="/settings" icon={faCog} title="Settings" />
+      <ul>
+        <li>
+          <NavigationButton to="/home" icon={faHome} title="Home" />
+        </li>
+        <li>
+          <NavigationButton
+            to="/subjects"
+            icon={faStickyNote}
+            title="Subjects"
+          />
+        </li>
+        <li>
+          <NavigationButton to="/settings" icon={faCog} title="Settings" />
+        </li>
+      </ul>
     </nav>
   );
 };
