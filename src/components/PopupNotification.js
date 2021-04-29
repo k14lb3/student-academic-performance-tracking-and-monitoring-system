4f9222b209ab74a3622237fd94537c99899a0a70
Kialbe <k14lb3@gmail.com>
2021-04-29 21:56:25 +0800

    Delete scss styles and migrate to tailwindcss, and change props

--- src/components/PopupNotification.js
+++ src/components/PopupNotification.js
@@ -1,21 +1,21 @@
 import { useEffect } from 'react';
 
-const PopupNotification = ({
-  popupState: { popup, setPopup },
-  message,
-  timeout,
-}) => {
+const PopupNotification = ({ popupState: { popup, setPopup }, timeout }) => {
   useEffect(() => {
     if (popup) {
       setTimeout(() => {
-        setPopup(false);
+        setPopup({ up: false, message: '' });
       }, timeout);
     }
   }, [popup, setPopup, timeout]);
 
   return (
-    <div className={`popup${popup ? ' popup--active' : ''}`}>
-      <p className="popup__message">{message}</p>
+    <div
+      className={`fixed bottom-5 py-3 px-8 bg-orange rounded text-gray${
+        popup ? ' block' : ' hidden '
+      }`}
+    >
+      <p className="text-xl">{popup.message}</p>
     </div>
   );
 };
