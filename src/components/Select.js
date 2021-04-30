ba8be1307da6a18d214523e3a0f179bf4e542ddf
Kialbe <k14lb3@gmail.com>
2021-04-30 14:19:59 +0800

    Remove top margin

--- src/components/Select.js
+++ src/components/Select.js
@@ -4,7 +4,7 @@ const Select = ({ className, defaultValue, onChange, children }, ref) => {
   return (
     <select
       ref={ref}
-      className={`bg-gray-50 px-3 py-2 mt-3 text-black xs:text-xs border border-black rounded${
+      className={`bg-gray-50 px-3 py-2 text-black xs:text-xs border border-black rounded${
         ` ${className}` || ''
       }`}
       defaultValue={defaultValue}
