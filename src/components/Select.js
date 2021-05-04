d340dd60e184cf6b5cb898e040af8cf39e0cfbcc
Kialbe <k14lb3@gmail.com>
2021-05-04 09:05:24 +0800

    Change border color

--- src/components/Select.js
+++ src/components/Select.js
@@ -4,8 +4,8 @@ const Select = ({ className, defaultValue, onChange, children }, ref) => {
   return (
     <select
       ref={ref}
-      className={`bg-gray-50 px-3 py-2 text-black xs:text-xs border border-black rounded${
-        ` ${className}` || ''
+      className={`bg-gray-50 px-3 py-2 text-black xs:text-xs border border-gray-50 rounded ${
+        className ? className : ''
       }`}
       defaultValue={defaultValue}
       onChange={onChange}
