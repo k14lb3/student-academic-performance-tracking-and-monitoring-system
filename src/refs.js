9e62bd90faeac19254a916db41779ab86d8c9440
Kialbe <k14lb3@gmail.com>
2021-05-10 19:50:45 +0800

    Add more documents references and fix typo

--- src/refs.js
+++ src/refs.js
@@ -4,11 +4,23 @@ export const REF = {
   USERS: () => db.collection('users'),
   USER: ({ user_uid }) => db.collection('users').doc(user_uid),
   USER_SUBJECTS: ({ user_uid }) =>
-    db.collection('users').doc(user_uid).collection('subjecs'),
+    db.collection('users').doc(user_uid).collection('subjects'),
+  USER_SUBJECT: ({ user_uid, subject_code }) =>
+    db
+      .collection('users')
+      .doc(user_uid)
+      .collection('subjects')
+      .doc(subject_code),
   USER_ARCHIVED_SUBJECTS: ({ user_uid }) =>
-    db.collection('users').doc(user_uid).collection('archived_subjecs'),
+    db.collection('users').doc(user_uid).collection('archived_subjects'),
   SUBJECTS: () => db.collection('subjects'),
   SUBJECT: ({ subject_code }) => db.collection('subjects').doc(subject_code),
   SUBJECT_STUDENTS: ({ subject_code }) =>
     db.collection('subjects').doc(subject_code).collection('students'),
+  SUBJECT_STUDENT: ({ subject_code, student_uid }) =>
+    db
+      .collection('subjects')
+      .doc(subject_code)
+      .collection('students')
+      .doc(student_uid),
 };
