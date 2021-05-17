9310cd5550e5e3fd446ea302099cf01d39fd42c6
Kialbe <k14lb3@gmail.com>
2021-05-18 00:14:43 +0800

    Add className prop

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,5 +1,11 @@
-const Label = ({ children }) => {
-  return <label className="block mb-3 xs:text-xs">{children}</label>;
+const Label = ({ className, children }) => {
+  return (
+    <label
+      className={`block mb-3 xs:text-xs${className ? ` ${className}` : ''}`}
+    >
+      {children}
+    </label>
+  );
 };
 
 export default Label;
