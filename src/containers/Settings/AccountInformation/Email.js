4e6e67ff70be3ec4d0d7a0206af4f0005e5dce66
Kialbe <k14lb3@gmail.com>
2021-05-18 00:04:16 +0800

    Refractor Code

--- src/containers/Settings/AccountInformation/Email.js
+++ src/containers/Settings/AccountInformation/Email.js
@@ -26,9 +26,7 @@ const Email = ({ setPopup }) => {
     return setUpdateButton(true);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
+  const handleUpdate = async () => {
     if (updateButton) {
       setError('');
       setUpdating(true);
@@ -48,7 +46,7 @@ const Email = ({ setPopup }) => {
       {loading ? (
         <Loader className="mx-auto mt-5" />
       ) : (
-        <form onSubmit={handleSubmit}>
+        <>
           <div className="p-5 xs:p-3 border border-orange-500 rounded">
             <Label>Email</Label>
             <Input
@@ -61,13 +59,14 @@ const Email = ({ setPopup }) => {
             {error && <Error error={error} />}
           </div>
           <Button
-            disabled={updating || !updateButton}
+            disabled={!updateButton}
             className="mt-5 ml-auto"
             hasLoader={{ loading: updating }}
+            onClick={handleUpdate}
           >
             <span className={updating ? 'invisible' : ''}>Update</span>
           </Button>
-        </form>
+        </>
       )}
     </>
   );
