558ef18f6b2985861747d0acb80bec56d5a4a41d
Kialbe <k14lb3@gmail.com>
2021-05-19 00:57:52 +0800

    Bring back updateTitle function lol

--- src/contexts/SubjectsContext.js
+++ src/contexts/SubjectsContext.js
@@ -15,6 +15,7 @@ export const ACTIONS = {
   ARCHIVE_SUBJECT: 'archive_subject',
   DELETE_CURRENT_SUBJECT: 'delete_current_subject',
   DELETE_ARCHIVED_SUBJECT: 'delete_archived_subject',
+  UPDATE_SUBJECT_TITLE: 'update_subject_title',
 };
 
 const doesSubjectExists = async (code) => {
@@ -85,10 +86,24 @@ const subjectsReducer = (subjects, action) => {
           (subject) => subject.code !== action.payload.code
         ),
       };
-    default:
+ case 'update_subject_title':
+      return {
+        current: subjects.current.map((subject) => {
+          if (subject.code === action.payload.code) {
+            return {
+              ...subject,
+              title: action.payload.title,
+            };
+          }
+          return subject;
+        }),
+        archived: [...subjects.archived],
+      };
+       default:
       return subjects;
   }
 };
+
 
 const SubjectsProvider = ({ children }) => {
   const { user } = useAuth();
@@ -336,6 +351,14 @@ const SubjectsProvider = ({ children }) => {
     };
   };
 
+  const updateTitle = async (code, title) => {
+    REF.SUBJECT({ subject_code: code }).update({ title: title });
+    subjectsDispatch({
+      type: ACTIONS.UPDATE_SUBJECT_TITLE,
+      payload: { code: code, title: title },
+    });
+  };
+
   useEffect(() => {
     if (!user) {
       subjectsDispatch({ type: ACTIONS.RESET_SUBJECTS });
@@ -350,6 +373,7 @@ const SubjectsProvider = ({ children }) => {
     createSubject,
     joinSubject,
     deleteSubject,
+    updateTitle,
   };
 
   return (
